a3446912ec4f7869d4bce7ef4c59891d770d34a4
Evilong <evilong_for_life@hotmail.com>
2018-05-22 20:52:46 +0100

    -Finished Settings Fragment

--- app/src/main/java/pt/up/fc/dcc/embeddedsystems/smarthousecontroller/fragment/SettingsFragment.java
+++ app/src/main/java/pt/up/fc/dcc/embeddedsystems/smarthousecontroller/fragment/SettingsFragment.java
@@ -6,8 +6,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 import pt.up.fc.dcc.embeddedsystems.smarthousecontroller.R;
 import pt.up.fc.dcc.embeddedsystems.smarthousecontroller.api.SettingsApi;
@@ -22,9 +29,12 @@ import retrofit2.Response;
 public class SettingsFragment extends Fragment {
 
     SettingsApi settingsApi;
-
-    public SettingsFragment() {
-        }
+    Setting setting;
+    ToggleButton toggle;
+    NumberPicker numberPicker1;
+    NumberPicker numberPicker2;
+    TextView setting_message;
+    Button btn_apply;
 
     public static android.support.v4.app.Fragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -37,44 +47,88 @@ public class SettingsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         settingsApi = RetrofitClientInstance.getRetrofitInstance().create(SettingsApi.class);
-        //The code bellow is just an example !!! Should be removed soon.
-        Setting setting = new Setting();
-        setting.setAutomatic(true);
-        setting.setThreshold(new BigDecimal(15.5));
-        Call<StatusResponse> call = settingsApi.setHomeSettings(setting);
-        call.enqueue(new Callback<StatusResponse>() {
+        Call<Setting> call = settingsApi.homeSettings();
+        call.enqueue(new Callback<Setting>() {
             @Override
-            public void onResponse(Call<StatusResponse> call, Response<StatusResponse> response) {
-                Call<Setting> call2 = settingsApi.homeSettings();
-                call2.enqueue(new Callback<Setting>() {
-                    @Override
-                    public void onResponse(Call<Setting> call, Response<Setting> response) {
-                        Log.d("CONA",response.body().toString());
-                    }
-
-                    @Override
-                    public void onFailure(Call<Setting> call, Throwable t) {
-
-                    }
-                });
+            public void onResponse(Call<Setting> call, Response<Setting> response) {
+                if(response.body() == null){
+                    setting = new Setting();
+                }
+                else setting = response.body();
+                if(setting.isAutomatic() == null) setting.setAutomatic(true);
+                if(setting.getThreshold() == null) setting.setThreshold(new BigDecimal(10.5));
+                toggle.setChecked(setting.isAutomatic());
+                numberPicker1.setValue(setting.getThreshold().intValue());
+                numberPicker2.setValue(setting.getThreshold().remainder(BigDecimal.ONE).movePointLeft(1).intValue());
+                getView().findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                setting_message.setVisibility(View.GONE);
             }
 
             @Override
-            public void onFailure(Call<StatusResponse> call, Throwable t) {
-
+            public void onFailure(Call<Setting> call, Throwable t) {
+                Log.e("SettingsFragment", t.getMessage());
             }
         });
-        if (getArguments() != null) {
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_settings, container, false);
+        view.findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+        setting_message = view.findViewById(R.id.settings_message);
+        toggle = view.findViewById(R.id.s_auto);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setting.automatic(isChecked);
+                enableApply();
+            }
+        });
+        numberPicker1 = view.findViewById(R.id.int_picker);
+        numberPicker1.setMinValue(0);
+        numberPicker1.setMaxValue(20);
+        numberPicker1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                enableApply();
+            }
+        });
+        numberPicker2 = view.findViewById(R.id.dec_picker);
+        numberPicker2.setMinValue(0);
+        numberPicker2.setMaxValue(99);
+        numberPicker2.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                enableApply();
+            }
+        });
+        btn_apply = view.findViewById(R.id.btn_apply);
+        btn_apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                BigDecimal bd = new BigDecimal(numberPicker1.getValue()+"."+numberPicker2.getValue());
+                bd.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                setting.setThreshold(bd);
+                Call<StatusResponse> call = settingsApi.setHomeSettings(setting);
+                call.enqueue(new Callback<StatusResponse>() {
+                    @Override
+                    public void onResponse(Call<StatusResponse> call, Response<StatusResponse> response) {
+                        setting_message.setVisibility(View.GONE);
+                        btn_apply.setEnabled(false);
+                    }
+                    @Override
+                    public void onFailure(Call<StatusResponse> call, Throwable t) {
+                        Log.e("SettingsFragment",t.getMessage());
+                        Toast.makeText(view.getContext(),t.toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+        return view;
     }
 
-
+    private void enableApply(){
+        setting_message.setVisibility(View.VISIBLE);
+        btn_apply.setEnabled(true);
+    }
 
 }
