002b5528dec896bc570dc269f02de3ea88ea6b52
Evilong <evilong_for_life@hotmail.com>
2018-05-23 01:30:10 +0100

    small fix

--- app/src/main/java/pt/up/fc/dcc/embeddedsystems/smarthousecontroller/fragment/MusicFragment.java
+++ app/src/main/java/pt/up/fc/dcc/embeddedsystems/smarthousecontroller/fragment/MusicFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.math.BigDecimal;
 
@@ -36,32 +37,43 @@ public class MusicFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         musicApi = RetrofitClientInstance.getRetrofitInstance().create(MusicApi.class);
-        updateMusicDisplay();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_music, container, false);
+        updateMusicDisplay(view);
         return view;
     }
 
-    private void updateMusicDisplay(){
-        getView().findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+    private void updateMusicDisplay(final View view){
+        view.findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
         Call<MusicPlayerStatus> call = musicApi.musicSummary();
         call.enqueue(new Callback<MusicPlayerStatus>() {
             @Override
             public void onResponse(Call<MusicPlayerStatus> call, Response<MusicPlayerStatus> response) {
                 musicPlayerStatus = response.body();
                 TextView music_title = getView().findViewById(R.id.text_view_music_title);
-                if(musicPlayerStatus.isStatus()) music_title.setText(musicPlayerStatus.getTrack().getName());
-                else music_title.setText("Not playing !");
+                if(musicPlayerStatus.isStatus() == null || !musicPlayerStatus.isStatus()){
+                    view.findViewById(R.id.btn_stop).setVisibility(View.GONE);
+                    view.findViewById(R.id.btn_play).setVisibility(View.VISIBLE);
+                    view.findViewById(R.id.btn_next).setVisibility(View.GONE);
+                    music_title.setText("Not playing !");
+                }
+                else {
+                    view.findViewById(R.id.btn_stop).setVisibility(View.VISIBLE);
+                    view.findViewById(R.id.btn_play).setVisibility(View.GONE);
+                    view.findViewById(R.id.btn_next).setVisibility(View.VISIBLE);
+                    music_title.setText(musicPlayerStatus.getTrack().getName());
+                }
                 getView().findViewById(R.id.loadingPanel).setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<MusicPlayerStatus> call, Throwable t) {
-                getView().findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                view.findViewById(R.id.loadingPanel).setVisibility(View.GONE);
                 Log.e("MusicFragment",t.getMessage());
+                Toast.makeText(view.getContext(),t.toString(),Toast.LENGTH_LONG).show();
             }
         });
     }
